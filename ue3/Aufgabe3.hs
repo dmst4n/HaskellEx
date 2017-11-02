1adf17003b4afaa998fcd6027ee7cfd67fbc8be7
dmstan <e1225178@student.tuwien.ac.at>
2017-11-02 21:53:40 +0100

    Ex3 Solution1,4

--- ue3/Aufgabe3.hs
+++ ue3/Aufgabe3.hs
@@ -22,3 +22,17 @@ isPrime :: [Integer] -> Integer -> Bool
 isPrime (p:ps) n
          | p*p > n = True 
          | otherwise = n `rem` p /= 0 && isPrime ps n
+
+-- Solution 1.4
+verflechten3 :: [Int] -> [Int] -> [Int] -> [Int]
+verflechten3 [] [] [] = []
+verflechten3 [] bs cs = verflechten bs cs 
+verflechten3 as [] cs = verflechten as cs 
+verflechten3 as bs [] = verflechten as bs 
+verflechten3 (a:as) (b:bs) (c:cs) = a : b : c : (verflechten3 as bs cs)
+
+-- Helper 1.4
+verflechten :: [Int] -> [Int] -> [Int]
+verflechten (n1:n1s) (n2:n2s) = n1 : n2 : (verflechten n1s n2s)
+verflechten [] (n2s) = n2s
+verflechten (n1s) [] = n1s
