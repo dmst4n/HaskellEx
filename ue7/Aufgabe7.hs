e8cf0b25296b97926db64ad08597661b2abb6ab4
dmstan <e1225178@student.tuwien.ac.at>
2017-12-02 19:02:19 +0100

    Finished 7.1

---
+++ ue7/Aufgabe7.hs
@@ -0,0 +1,102 @@
+import Data.List (sort, sortBy, nub)
+import Data.Ord (comparing)
+
+--Solution 6.1
+
+type Wahrheitswert = Bool
+
+data VName = N1 | N2 | N3 | N4 | N5 deriving (Eq,Ord,Enum,Show)
+
+newtype Variable = Var VName deriving (Eq,Ord,Show)
+
+instance Enum Variable where
+  fromEnum (Var name) = fromEnum name
+  toEnum n = Var (toEnum n :: VName)
+
+
+data Ausdruck = K Wahrheitswert
+		| V Variable
+		| Nicht Ausdruck
+		| Und Ausdruck Ausdruck
+		| Oder Ausdruck Ausdruck
+        | Impl Ausdruck Ausdruck
+        | Esgibt Variable Ausdruck
+        | Fueralle Variable Ausdruck 
+		deriving (Eq,Show)
+
+type Belegung = Variable -> Wahrheitswert -- Total definierte Abbildung
+
+test :: Belegung
+test (Var N1) = True
+test (Var N2) = False
+test (Var N3) = True
+test (Var N4) = False
+test (Var N5) = True
+
+evaluiere :: Ausdruck -> Belegung -> Wahrheitswert
+evaluiere (K True)  f = True
+evaluiere (K False) f = False
+evaluiere (V var1) f = f var1
+evaluiere (Nicht aus) f  = not $ evaluiere aus f
+evaluiere (Und aus1 aus2) f = (evaluiere aus1 f) && (evaluiere aus2 f)
+evaluiere (Oder aus1 aus2) f = (evaluiere aus1 f) || (evaluiere aus2 f) 
+evaluiere (Impl aus1 aus2) f = (evaluiere (Nicht aus1) f) || (evaluiere aus2 f)
+evaluiere (Esgibt var1 aus) f = (evaluiere aus (f1 f var1)) || (evaluiere aus (f2 f var1))
+    where 
+        f1 :: Belegung -> Variable -> Belegung
+        f1 f var1 var2  
+            | var1 == var2 = True
+            | otherwise = (f var2)
+
+        f2 :: Belegung -> Variable -> Belegung
+        f2 f var1 var2  
+            | var1 == var2 = False
+            | otherwise = (f var2)
+evaluiere (Fueralle var1 aus) f = (evaluiere aus (f1 f var1)) && (evaluiere aus (f2 f var1))
+    where 
+        f1 :: Belegung -> Variable -> Belegung
+        f1 f var1 var2  
+            | var1 == var2 = True
+            | otherwise = (f var2)
+
+        f2 :: Belegung -> Variable -> Belegung
+        f2 f var1 var2  
+            | var1 == var2 = False
+            | otherwise = (f var2)
+
+ist_tautologie :: Ausdruck -> Ausdruck -> Bool
+ist_tautologie aus1 aus2 = evaluiere (Fueralle (Var N1) (Fueralle (Var N2) (Fueralle (Var N3) (Fueralle (Var N4) (Fueralle (Var N5) (Oder (Und aus1 aus2) (Nicht (Und aus1 aus2)))))))) f
+    where 
+        f :: Belegung
+        f _ = True
+
+schreibe :: Ausdruck -> String
+schreibe (K True) = "wahr"
+schreibe (K False) = "falsch"
+schreibe (V (Var n)) = show n
+schreibe (Nicht a) = "(" ++ "neg" ++ " " ++ schreibe a ++ ")"
+schreibe (Und a1 a2) = "(" ++ schreibe a1 ++ " " ++ "und" ++ " " ++ schreibe a2 ++ ")"
+schreibe (Oder a1 a2) = "(" ++ schreibe a1 ++ " " ++ "oder" ++ " " ++ schreibe a2 ++ ")" 
+schreibe (Impl a1 a2) = "(" ++ schreibe a1 ++ " " ++ "=>" ++ " " ++ schreibe a2 ++ ")" 
+schreibe (Esgibt v a) = "(" ++ "EG" ++ " " ++  schreibe (V v) ++ "." ++ " " ++ schreibe a ++")"
+schreibe (Fueralle v a) = "(" ++ "FA" ++ " " ++  schreibe (V v) ++ "." ++ " " ++ schreibe a ++")"
+
+nnf :: Ausdruck -> Ausdruck 
+nnf (Nicht (K True)) = (K False)
+nnf (Nicht (K False)) = (K True)
+nnf (Nicht (V (Var n))) = (Nicht (V (Var n)))
+nnf (Nicht (Nicht a)) = nnf a
+nnf (Nicht (Und a1 a2)) = (Oder (nnf (Nicht a1)) (nnf (Nicht a2)))
+nnf (Nicht (Oder a1 a2)) = (Und (nnf (Nicht a1)) (nnf (Nicht a2)))
+nnf (Nicht (Impl a1 a2)) = nnf $ Nicht $ Oder (nnf (Nicht a1)) (nnf a2)
+nnf (Nicht (Esgibt v a)) = Fueralle v $ nnf $ Nicht a
+nnf (Nicht (Fueralle v a)) = Esgibt v $ nnf $ Nicht a
+nnf (K True) = (K True)
+nnf (K False) = (K False)
+nnf (V (Var n)) = (V (Var n))
+nnf (Und a1 a2) = Und (nnf a1) (nnf a2)
+nnf (Oder a1 a2) = Oder (nnf a1) (nnf a2)
+nnf (Impl a1 a2) = Impl (nnf a1) (nnf a2)
+nnf (Esgibt v a) = (Esgibt v (nnf a))
+nnf (Fueralle v a) = (Esgibt v (nnf a)) 
+
